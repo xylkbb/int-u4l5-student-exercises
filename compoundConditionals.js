b6a828fdaacae472c48fa96630630d04480d4255
coeeri001 <coeeri001@warren.k12.in.us>
2025-06-17 13:30:38 -0400

    commit

--- compoundConditionals.js
+++ compoundConditionals.js
@@ -2,8 +2,8 @@
 
 // 1. Create a secret username and password.
 //  - The variables are declared already. Update the values.
-let secretUsername = ("");
-let secretPassword = ("");
+let secretUsername = ("God");
+let secretPassword = ("Dog");
 let button = document.querySelector("button");
 
 button.addEventListener("click", function() {
@@ -12,9 +12,22 @@ button.addEventListener("click", function() {
     let message = document.querySelector(".message");
 
     // 2. Create a compound conditional statement to check for the following:
+
+    if (username === secretUsername && password === secretPassword) {
+        message.innerHTML = "Success!";
+    }
     //  - If the username AND password is correct, update the text of the message div to say "Success!"
     
     // CODE SOLO
+    if (username === secretUsername && password !== secretPassword) {
+        message.innerHTML = "Wrong password - be careful, don't mess this up too many times.";
+    } else if (username !== secretUsername && password === secretPassword) {
+        message.innerHTML = "Wrong username - is there a typo?";
+    } else if (username !== secretUsername && password !== secretPassword) {
+        message.innerHTML = "Incorrect username and password. Hmm. Suspicious.";
+    } else if (username === "" || password === "") {
+        message.innerHTML = "You need to enter SOMETHING.";
+    }
     // 3. Create additional compound conditionals that check for the following:
     //  - If the username is correct AND the password is incorrect, update the text of the message div to say "Wrong password - be careful, don't mess this up too many times."
     //  - If the password is correct AND the username is incorrect, update the text of the message div to say "Wrong username - is there a typo?"
--- script.js
+++ script.js
@@ -6,6 +6,16 @@ button.addEventListener("click", function() {
     if (waterTemp >= 212) {
    document.querySelector("p").innerHTML="Boiling!";
     }
+    else if (waterTemp >= 155) {
+        document.querySelector("p").innerHTML="Hot!";
+    }
+    else if (waterTemp > 32) {
+        document.querySelector("p").innerHTML="It's ok to drink!";
+    }
+    else {
+        document.querySelector("p").innerHTML="Frozen!";
+
+    }
     // 1. Add an else-if and else so that:
     //  - If the water temp is greater than 32°, the user knows it's ok to drink.
     //  - If the water temp is 32° or less, the user knows the water is frozen.
